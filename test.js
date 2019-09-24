9e810da7f3eb178d4de740c977fa62294f2a94ba
Jerry <fenturechance@gmail.com>
2019-09-24 15:33:20 +0800

    cheerio

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
 let https = require('https');
 let http = require('http');
+let request = require('request');
 let fs = require('fs');
 let circular = require('circular');
+const cheerio = require('cheerio')
 let server = http.createServer((req,resp) => {
-    https.get('https://news.google.com/?hl=zh-TW&gl=TW&ceid=TW:zh-Hant',(rs) => {
-        if(rs.statusCode !== 200){
+    request('https://news.google.com/?hl=zh-TW&gl=TW&ceid=TW:zh-Hant',(err, res, body) => {
+        if(err){
             resp.writeHead(404);
             resp.write('Contents you are looking are Not Found');
         }else{
             resp.writeHead(200,{ 'Content-Type': 'text/html' });
-            let str = JSON.stringify(rs, circular())
+            const $ = cheerio.load(body)
+            let str = '';
+            $('body').each(function(i, elem) {
+                str += $(this).text().split('\n');
+            })
             resp.write(str);
         }
         resp.end();
